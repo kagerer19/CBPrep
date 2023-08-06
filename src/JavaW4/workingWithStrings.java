e3de2fa0db4945f0162b91c82a543b78a694070e
Alexander kagerer <kagerer19@gmail.com>
2023-08-06 11:11:42 +0200

    DAY 1: Working with Strings

---
+++ src/JavaW4/workingWithStrings.java
@@ -0,0 +1,18 @@
+package JavaW4;
+
+public class workingWithStrings {
+    public static void main(String[] args) {
+        String greeting = "Hello World";
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println("-----------------------------------------------------");
+            System.out.println(greeting + " is " + greeting.length() + " characters long");
+            System.out.println("Upper case: " + greeting.toUpperCase());
+            System.out.println("Lower case: " + greeting.toLowerCase());
+            System.out.println(greeting.replace("World", "CodersBay"));
+            System.out.println("-----------------------------------------------------");
+            System.out.println();
+        }
+
+    }
+}
