dd170743a2919463f5706163803d0ae4d4821de0
Alexander kagerer <kagerer19@gmail.com>
2023-08-06 11:02:55 +0200

    DAY 1: Simple Calculations

---
+++ src/JavaW4/simpleCalculations.java
@@ -0,0 +1,37 @@
+package JavaW4;
+
+public class simpleCalculations {
+    public static void main(String[] args) {
+        //Exercise 1.2
+
+        int a = 137;
+        int b = 231;
+
+        double fractionOne = 137.0;
+        double fractionTwo = 490.0;
+
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b;
+        int quotient = b / a;
+
+        double sumFractional = fractionOne + fractionTwo;
+        double diffFractional = fractionOne - fractionTwo;
+        double productFractional = fractionOne * fractionTwo;
+        double quotientFractional = fractionTwo / fractionOne;
+
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(quotient);
+
+
+        System.out.printf("Sum: %.2f %n", sumFractional);
+        System.out.printf("Difference: %.2f %n", diffFractional);
+        System.out.printf("Product %.2f %n", productFractional);
+        System.out.printf("Quotient: %.2f %n", quotientFractional);
+
+
+    }
+}
