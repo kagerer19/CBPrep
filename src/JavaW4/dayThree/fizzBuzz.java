e7adce3ee4abcda62256f335deb1f1d15f721cc2
Alexander kagerer <kagerer19@gmail.com>
2023-08-07 10:06:40 +0200

    DAY 3: Simple FizzBuzz game

---
+++ src/JavaW4/dayThree/fizzBuzz.java
@@ -0,0 +1,19 @@
+package JavaW4.dayThree;
+
+public class fizzBuzz {
+    public static void main(String [] args) {
+
+        for (int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz" + "(" + i + ")");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz" + "(" + i + ")");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz" + "(" + i + ")");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
