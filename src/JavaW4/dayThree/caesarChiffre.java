579a1aa3f547d1643fca97ad6d4387d2dcefd92a
Alexander kagerer <kagerer19@gmail.com>
2023-08-07 11:36:01 +0200

    DAY 3: Write a program that encrypts a given message from the console

---
+++ src/JavaW4/dayThree/caesarChiffre.java
@@ -0,0 +1,28 @@
+package JavaW4.dayThree;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class caesarChiffre {
+    public static void main(String[] args) {
+        Random r = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+
+        //encryption
+        int key = r.nextInt(1, 26);
+
+        System.out.println("Enter something you would like to encrypt: ");
+        String textToEncrypt = scanner.nextLine();
+
+        char[] chars = textToEncrypt.toCharArray();
+
+        System.out.println("Your encrypted message is safe with us!");
+        System.out.println("---------------------------------------->");
+        for (char c : chars) {
+            c += key;
+            System.out.print(c);
+        }
+        System.out.println("\n---------------------------------------->");
+    }
+}
