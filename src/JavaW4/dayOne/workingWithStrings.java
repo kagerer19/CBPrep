3fa6236962b6338f90381270873c79fb036dbfbc
Alexander kagerer <kagerer19@gmail.com>
2023-08-08 17:21:29 +0200

    DAY 1: Practise with strings

--- src/JavaW4/dayOne/workingWithStrings.java
+++ src/JavaW4/dayOne/workingWithStrings.java
@@ -2,17 +2,16 @@ package JavaW4.dayOne;
 
 public class workingWithStrings {
     public static void main(String[] args) {
-        String greeting = "Hello World";
+        String greeting = "Hello World ";
 
-        for (int i = 0; i < 15; i++) {
-            System.out.println("-----------------------------------------------------");
-            System.out.println(greeting + " is " + greeting.length() + " characters long");
-            System.out.println("Upper case: " + greeting.toUpperCase());
-            System.out.println("Lower case: " + greeting.toLowerCase());
-            System.out.println(greeting.replace("World", "CodersBay"));
-            System.out.println("-----------------------------------------------------");
-            System.out.println();
-        }
+        System.out.println("-----------------------------------------------------");
+        System.out.println(greeting.strip() + " is " + greeting.length() + " characters long");
+        System.out.println("Upper case: " + greeting.toUpperCase());
+        System.out.println("Lower case: " + greeting.toLowerCase());
+        System.out.println(greeting.replace("World", "CodersBay").strip());
+        System.out.println("-----------------------------------------------------");
 
+        System.out.println((greeting.strip() + "\n").repeat(15));
     }
 }
+// stringLead needs to be implemented
