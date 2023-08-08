5cbaa10f49c85e925a39e7a382d34afda7f7d4eb
Alexander kagerer <kagerer19@gmail.com>
2023-08-08 17:19:58 +0200

    DAY 3: Create a program that takes numbers from a user and prints the max number of the given numbers

--- src/JavaW4/dayThree/maximaCalculation.java
+++ src/JavaW4/dayThree/maximaCalculation.java
@@ -1,6 +1,7 @@
 package JavaW4.dayThree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class maximaCalculation {
@@ -8,7 +9,6 @@ public class maximaCalculation {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> userInputHistory = new ArrayList<>();
 
-        int sum = 0;
         String userInput;
 
         System.out.println("Provide me with numbers, anything you can think of!");
@@ -16,10 +16,12 @@ public class maximaCalculation {
         while (!(userInput = scanner.next()).equalsIgnoreCase("q")) {
             int userNum = Integer.parseInt(userInput);
             userInputHistory.add(userNum);
-            sum += userNum;
             System.out.println("Provide me with numbers, anything you can think of!");
         }
-
-        System.out.println("Sum of " + userInputHistory + " Total: " + sum);
+        if (Collections.max(userInputHistory) > 0) {
+            System.out.println("Your history " + userInputHistory + " Max number: " + Collections.max(userInputHistory));
+        } else if(Collections.max(userInputHistory) < 0) {
+            System.out.println("Your history " + userInputHistory + " Max number: " + Collections.min(userInputHistory));
+        }
     }
 }
