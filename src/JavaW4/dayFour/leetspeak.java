50830fbe1c0ea23acde4bfe778186db98dc909ac
Alexander kagerer <kagerer19@gmail.com>
2023-08-10 14:47:59 +0200

    DAY 4: Fix Leet speak to take numbers and special charters

--- src/JavaW4/dayFour/leetspeak.java
+++ src/JavaW4/dayFour/leetspeak.java
@@ -8,7 +8,7 @@ public class leetspeak {
 
         char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         char[] leetChars = {'@', '8', '(', 'D', '3', 'F', '6', '#', '!', 'J', 'K', '1', 'M', 'N', '0', 'P', 'Q', 'R', '$', '7', 'U', 'V', 'W', 'X', 'Y', '2'};
-        String leetSpeakPhrase = "";
+        StringBuilder leetSpeakPhrase = new StringBuilder();
 
         System.out.println("Enter the phrase you would like to leet: ");
         String textToEncrypt = scanner.nextLine().toUpperCase();
@@ -16,11 +16,11 @@ public class leetspeak {
         for (int i = 0; i < textToEncrypt.length(); i++) {
             char temp = textToEncrypt.charAt(i);
             if (Character.isDigit(temp)) {
-                leetSpeakPhrase += temp;
+                leetSpeakPhrase.append(temp);
             } else {
                 for (int j = 0; j < alphabet.length; j++) {
                     if (temp == alphabet[j]) {
-                        leetSpeakPhrase += leetChars[j];
+                        leetSpeakPhrase.append(leetChars[j]);
                     }
                 }
             }
