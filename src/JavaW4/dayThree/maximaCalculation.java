62b92a0214daa6f82bda3385ab4d87b7160977d7
Alexander kagerer <kagerer19@gmail.com>
2023-08-10 14:46:56 +0200

    DAY 3: update maxima

--- src/JavaW4/dayThree/maximaCalculation.java
+++ src/JavaW4/dayThree/maximaCalculation.java
@@ -18,10 +18,7 @@ public class maximaCalculation {
             userInputHistory.add(userNum);
             System.out.println("Provide me with numbers, anything you can think of!");
         }
-        if (Collections.max(userInputHistory) > 0) {
-            System.out.println("Your history " + userInputHistory + " Max number: " + Collections.max(userInputHistory));
-        } else if(Collections.max(userInputHistory) < 0) {
-            System.out.println("Your history " + userInputHistory + " Max number: " + Collections.min(userInputHistory));
-        }
+        System.out.println("Your history " + userInputHistory + " Max number: " + Collections.max(userInputHistory));
+
     }
 }
